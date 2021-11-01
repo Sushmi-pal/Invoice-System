8690844a4e4bd82f1d4eaf0ef73bee633715197c
Sushmita <sushmipalikhe97@gmail.com>
2021-11-01 13:55:49 +0545

    # Please include a useful commit message! Signed-off-by: Sushmita <sushmipalikhe97@gmail.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 use OwenIt\Auditing\Contracts\Auditable;
 
-class User extends Authenticatable implements Auditable
+class User extends Authenticatable implements Auditable, MustVerifyEmail
 {
     use HasFactory, Notifiable, HasRoles, \OwenIt\Auditing\Auditable;
 
